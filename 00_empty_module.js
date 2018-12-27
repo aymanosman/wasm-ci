70bf85a1693f68ba5c43b3d9ca0c397a6f9dda18
Ayman Osman <aymano.osman@gmail.com>
2018-12-27 06:39:03 +0000

    wip

--- 00_empty_module.js
+++ 00_empty_module.js
@@ -1,4 +1,3 @@
 let file = new Buffer('\u0000asm\u0001\u0000\u0000\u0000\u0001\u0001\u0000')
 let wasm = new WebAssembly.Module(file);
-console.log(wasm)
-// => Module {}
+console.log(wasm) // => Module {}
